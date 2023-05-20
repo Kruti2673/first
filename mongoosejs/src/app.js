3e8cc887760482c1e203fa8961c61ca74bd5ab11
Kruti2673 <krutivadaliya01@gmail.com>
2023-05-20 10:33:48 +0530

    mongoose first code

---
+++ mongoosejs/src/app.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');                                       //express
+mongoose.connect('mongodb://127.0.0.1:27017/test');                         //app & port init
+
+const Cat = mongoose.model('Cat', { name: String });
+
+const kitty = new Cat({ name: 'Zildjian' });
+kitty.save().then(() => console.log('meow'));
+
